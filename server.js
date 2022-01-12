534bdcb629c9553625962b94d2569b58a3f4b7b5
nex2308 <mk.nex23@gmail.com>
2022-01-12 20:35:26 +0100

    zadanie2

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje 
 
 app.get("/", function (req, res) {
     res.send("<h1>my app on heroku - zadanie !</h1>")
-    const obj = {
-        imie: "Mikołaj", nazwisko: "Konieczny", klasa: "3i1", grupa: "gr.1"
-    }
-    res.send("<p>" + obj + "</p>")
 })
 
 app.listen(PORT, function () {
