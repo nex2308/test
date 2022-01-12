51b4db6f45ff590a427bb3b736b23bb1d3e419a0
nex2308 <mk.nex23@gmail.com>
2022-01-12 20:16:55 +0100

    commit zadanie

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
-    res.send("<h1>my app on heroku - kolejna mała zmiana!</h1>")
+    res.send("<h1>my app on heroku - zadanie !</h1>")
+    const obj = {
+        imie: "Mikołaj", nazwisko: "Konieczny", klasa: "3i1", grupa: "gr.1"
+    }
+    res.send("<p>" + obj + "</p>")
 })
 
 app.listen(PORT, function () {
